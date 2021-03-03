305de6eb381bdcfc0f70cd58691239ec712cbdb7
Melisa Ailén Onofri <onofrimelisa@gmail.com>
2021-03-03 11:10:31 -0300

    feat: fix reference to repository interface in the services

--- src/main/java/com/bootcamp/spring/service/CaloriesCalculatorService.java
+++ src/main/java/com/bootcamp/spring/service/CaloriesCalculatorService.java
@@ -1,11 +1,11 @@
 package com.bootcamp.spring.service;
 
+import com.bootcamp.spring.interfaces.ICaloriesCalculatorRepository;
 import com.bootcamp.spring.interfaces.ICaloriesCalculatorService;
 import com.bootcamp.spring.dto.ResponseDTO;
 import com.bootcamp.spring.model.Food;
 import com.bootcamp.spring.model.Ingredient;
 import com.bootcamp.spring.model.IngredientsWithCalories;
-import com.bootcamp.spring.repository.CaloriesCalculatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +14,7 @@ import java.util.List;
 @Service
 public class CaloriesCalculatorService implements ICaloriesCalculatorService {
     @Autowired
-    private CaloriesCalculatorRepository caloriesCalculatorRepository;
+    private ICaloriesCalculatorRepository caloriesCalculatorRepository;
 
     @Override
     public ResponseDTO generateResponse(Food food) {
